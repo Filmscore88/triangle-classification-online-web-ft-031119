baf63b67374bb28d6390613941bb1a7308d722dd
Alberto Morales <cmtmorales24@gmail.com>
2019-03-28 12:54:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,8 @@
 class Triangle
-attr_accessor
-  
-  
+attr_accessor :length2, :length2, :length3
+
+def initialize(length1, length2, length3)
+@length1=length1 
   
   
 class TriangleError < StandardError
