ab489f7b5cbaa8b178affbb054b2a5800ff43fd6
Alberto Morales <cmtmorales24@gmail.com>
2019-03-28 13:03:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,12 @@
+require 'pry'
 class Triangle
 attr_accessor :length2, :length2, :length3
 
 def initialize(length1, length2, length3)
 @triangle_sides=[]
-@triangle_sides
+@triangle_sides.push(length1, length2, length3)
+binding.pry
+end 
   
   
 class TriangleError < StandardError
